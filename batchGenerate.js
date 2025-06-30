d8fafbbfeb0339a7cb3087c2eae076d531d580c9
nodoubt4027 <nodoubt4027@gmail.com>
2025-06-30 21:07:52 +0700

    Update batchGenerate.js

--- batchGenerate.js
+++ batchGenerate.js
@@ -7,9 +7,7 @@ const path = require('path');
   if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
 
   const tokenIds = [
-    3049, 296, 301, 334, 434, 445, 1061, 1312, 1431, 1638,
-    1718, 1756, 1995, 1997, 1998, 2174, 3039
-  ];
+    434, 1638 ];
 
   for (const tokenId of tokenIds) {
     try {
